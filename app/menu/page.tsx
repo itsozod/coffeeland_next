85310a9925028f95d485d6aed9797c187acce981
Ozod <o.bukhorizoda@mail.ru>
2024-08-16 23:33:13 +0500

    chore: fix the build issue

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,3 +1,30 @@
 export const dynamic = "force-dynamic";
-import Menu from "@/src/pages/menuPage/ui/content";
-export default Menu;
+import { getCoffees } from "@/src/entities/coffees/api";
+import CoffeeCard from "@/src/entities/coffees/CoffeeCard";
+import { Params } from "@/src/entities/coffees/params.types";
+import PaginationLayout from "@/src/pages/menuPage/ui/Pagination/Pagination";
+import Search from "@/src/pages/menuPage/ui/Search/Search";
+import { Coffee } from "@/src/shared/types/types";
+import React from "react";
+import classes from "./page.module.css";
+
+export default async function Menu({ searchParams }: { searchParams: Params }) {
+  const coffees: Coffee[] = await getCoffees(
+    searchParams?.name,
+    searchParams?.page
+  );
+
+  return (
+    <>
+      <Search />
+      <div className={classes["full_container"]}>
+        <div className={classes["coffeecard_container"]}>
+          {coffees?.map((coffee: Coffee) => {
+            return <CoffeeCard key={coffee?.id} coffee={coffee} />;
+          })}
+        </div>
+        <PaginationLayout />
+      </div>
+    </>
+  );
+}
